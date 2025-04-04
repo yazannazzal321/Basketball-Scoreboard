c3f4ec6ac06ffbe2089c92ba2cba92e58121a786
yazannazzal <134092956+yazannazzal321@users.noreply.github.com>
2025-04-04 16:59:00 +0200

    Commit initial files

---
+++ index.js
@@ -0,0 +1,34 @@
+let HomeRus = document.getElementById("HomeScoreboard");
+let HomeScore = 0;
+let GuestRus = document.getElementById("GuestScoreboard");
+let GuestScore = 0;
+
+// Function to increase the Home score by 1, 2, or 3 points
+function increaseHomeScore1() {
+  HomeScore += 1;
+  HomeRus.textContent = HomeScore;
+}
+function increaseHomeScore2() {
+  HomeScore += 2;
+  HomeRus.textContent = HomeScore;
+}
+function increaseHomeScore3() {
+  HomeScore += 3;
+  HomeRus.textContent = HomeScore;
+}
+
+/////////////////////////////////////////////////////////////////////
+// Function to increase the Home score by 1, 2, or 3 points
+function increaseGuestScore1() {
+  GuestScore += 1;
+  GuestRus.textContent = GuestScore;
+}
+GuestRus;
+function increaseGuestScore2() {
+  GuestScore += 2;
+  GuestRus.textContent = GuestScore;
+}
+function increaseGuestScore3() {
+  GuestScore += 3;
+  GuestRus.textContent = GuestScore;
+}
